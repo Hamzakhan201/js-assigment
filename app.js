3aa412a10767c952774c91151a856e912774849b
Hamzakhan201 <hamza.khan23472@gmai.com>
2023-04-01 04:20:48 +0500

    Work Complete

---
+++ app.js
@@ -0,0 +1,85 @@
+alert("Error! Please Eanter A valid Password");
+
+var line1 = "Walcome to JS Land...";
+var line2 = "Happy coding";
+var message = line1 + "\n" + line2;
+alert(message);
+
+var name = "Hamza khan";
+alert(name);
+
+var age = "20 years old";
+alert(age);
+
+var email = "hamzakhan123@example.com";
+alert(email);
+
+document.write("I can write HTML contant through JavaScript")
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+var age = "20 years old";
+alert(age);
+
+var visit = "You have visited this site 14 times";
+alert(visit);
+
+var birthyear = ("my birth year is 1990");
+alert(birthyear);
+
+// var myValue //(legal)
+// var 123val =4 //(illagl)
+// var $abc= "hello world" //(legal)
+// var _abc= "hello world" //(legal)
+// var alert = "hey"
+
+var a = 4;
+var b = 6;
+var c = a + b;
+alert(c)
+
+var a = 3;
+var b = b;
+var c = a + b;
+alert (c)
+
+var a = 9;
+var b = "abc";
+var c = a + b;
+alert(c)
+
+
+var a = 9;
+var b = 9;
+var c = a * b + a;
+alert(c)
+
+var a = 6;
+var b = 3;
+var c = a / b;
+alert(c)
+
+var a = 6;
+var b = 9;
+var c = a - b;
+alert(c)
+
+var a = 7;
+var b = 7;
+var c = 8;
+var d = (a * b) + c;
+alert(d)
+
+var a = 9;
+var b = ++a;
+alert(b);
+
+var a = 5;
+var b = --a;
+alert(b)
+
+var a;
+a = 9;
+var b;
+b= ++a;
+alert(a) 
+alert(b) 
